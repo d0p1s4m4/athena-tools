d61f708528cacac5b408825dc03961aa9d15f68b
d0p1 <contact@d0p1.eu>
2022-06-28 13:35:19 +0200

    wip

--- disas.pas
+++ disas.pas
@@ -13,12 +13,17 @@ const
 
 procedure printIFormat(decodedInstr: String; ra: UInt8; rb: UInt8; imm: UInt16);
 begin
-    writeLn(decodedInstr, ', ', REGS[ra], ', ', REGS[rb], ', ', imm);
+    writeLn(decodedInstr, ' ', REGS[ra], ', ', REGS[rb], ', ', imm);
 end;
 
 procedure printRFormat(decodedInstr: String; ra: UInt8; rb: UInt8; rc: UInt8);
 begin
-    writeLn(decodedInstr, ', ', REGS[ra], ', ', REGS[rb], ', ', REGS[rc]);
+    writeLn(decodedInstr, ' ', REGS[ra], ', ', REGS[rb], ', ', REGS[rc]);
+end;
+
+procedure printRShiftFormat(decodedInstr: String; ra: UInt8; rb: UInt8; shmat: UInt8);
+begin
+    writeLn(decodedInstr, ' ', REGS[ra], ', ', REGS[rb], ', ', shmat);
 end;
 
 procedure printJFormat(decodedInstr: String; ra: UInt8; joffset: UInt32);
@@ -26,25 +31,49 @@ begin
     writeLn(decodedInstr, ' ', REGS[ra], ', ', joffset);
 end;
 
-procedure decodeFunc(ra: UInt8; rb: UInt8; rc: UInt8; func: UInt8);
+procedure decodeFunc(ra: UInt8; rb: UInt8; rc: UInt8; shamt: Uint8; func: UInt8);
 begin
     case func of
-        %000000: writeLn('nop');
         %000001: printRFormat('add', ra, rb, rc);
         %000010: printRFormat('addu', ra, rb, rc);
-        %000011: printRFormat('sub', ra, rb, rc);
-        %000100: printRFormat('subu', ra, rb, rc);
         %000101: printRFormat('and', ra, rb, rc);
         %000110: printRFormat('div', ra, rb, rc);
         %000111: printRFormat('divu', ra, rb, rc);
-        %001000: printRFormat('mult', ra, rb, rc);
-        %001001: printRFormat('multu', ra, rb, rc);
+        %010011: printRFormat('mod', ra, rb, rc);
+        %010100: printRFormat('modu', ra, rb, rc);
+        %001000: writeLn('mult ', REGS[ra], ' ', REGS[rb], ' ', REGS[rc], ' ', REGS[shamt]);
+        %001001: writeLn('multu ', REGS[ra], ' ', REGS[rb], ' ', REGS[rc], ' ', REGS[shamt]);
+        %000000: writeLn('nop');
         %001010: printRFormat('nor', ra, rb, rc);
         %001011: printRFormat('or', ra, rb, rc);
-        %111111: writeLn('syscall');
+        %001101: printRShiftFormat('ssl', ra, rb, shamt);
+        %010000: printIFormat('sslr', ra, rb, rc);
+        %001111: printRShiftFormat('sra', ra, rb, shamt);
+        %010001: printRFormat('srar', ra, rb, rc);
+        %001110: printRShiftFormat('srl', ra, rb, shamt);
+        %010010: printRFormat('srlr', ra, rb, rc);
+        %000011: printRFormat('sub', ra, rb, rc);
+        %000100: printRFormat('subu', ra, rb, rc);
+        %001100: printRFormat('xor', ra, rb, rc);
+        %111111: writeLn('trap');
     else
         writeLn('???');
     end;
+end;
+
+procedure decodeLoadStore(opcode: Uint8; ra: Uint8; rb: Uint8; ssize: Uint8; offset: UInt16);
+begin
+    if (opcode = %001001) or (opcode = %001100) then write('l') else write('s');
+    case ssize of
+        %00: write('b');
+        %01: write('h');
+        %10: write('w');
+    end;
+    if opcode = %001100 then write('u');
+    if (opcode = %001001) or (opcode = %001100) then
+        writeLn(' ', REGS[ra], ', ', offset, '(', REGS[rb], ')')
+    else
+        writeLn(' ', offset, '(', REGS[ra], '), ', REGS[rb]);
 end;
 
 procedure decodeInstr(instr: UInt32);
@@ -58,6 +87,7 @@ var
     imm: UInt16;
     joffset: UInt32;
     lssize: UInt8;
+    lsoffset: UInt16;
 begin
 {$IFDEF ENDIAN_LITTLE}
     instr := SwapEndian(instr);
@@ -71,19 +101,14 @@ begin
     shmat := (instr shr 21) and $1F;
     imm := (instr shr 16) and $FFFF;
     joffset := (instr shr 11) and $1FFFFF;
+    lssize := (instr shr 16) and $2;
+    lsoffset := (instr shr 18);
 
     case opcode of
-        %000000: decodeFunc(ra, rb, rc, func);
+        %000000: decodeFunc(ra, rb, rc, shmat, func);
         %000001: printIFormat('addi', ra, rb, imm);
         %000010: printIFormat('addiu', ra, rb, imm);
-        %000011: printIFormat('subi', ra, rb, imm);
-        %000100: printIFormat('subiu', ra, rb, imm);
         %000101: printIFormat('andi', ra, rb, imm);
-        %000110: printIFormat('ori', ra, rb, imm);
-        %000111: printIFormat('xori', ra, rb, imm);
-        %001010: printJFormat('jmp', ra, joffset);
-        %001011: writeLn('lih ', ERGS[ra], ', ', imm);
-        %001101: printJFormat('call', ra, joffset);
         %001110:
         begin
             if rb <> 0 then
@@ -92,11 +117,11 @@ begin
                 writeLn('beqz r', ra, ', ', imm)
             else writeLn('b ', imm);
         end;
-        %001111:
+        %010001:
         begin
             if rb <> 0 then
-                printIFormat('bne', ra, rb, imm)
-            else writeLn('bnez ', REGS[ra], ', ', imm);
+                printIFormat('bgt', ra, rb, imm)
+            else writeLn('bgtz ', REGS[ra], ', ', imm);
         end;
         %010000:
         begin
@@ -104,12 +129,22 @@ begin
                 printIFormat('blt', ra, rb, imm)
             else writeLn('bltz ', REGS[ra], ', ', imm);
         end;
-        %010001:
+        %001111:
         begin
             if rb <> 0 then
-                printIFormat('bgt', ra, rb, imm)
-            else writeLn('bgtz ', REGS[ra], ', ', imm);
+                printIFormat('bne', ra, rb, imm)
+            else writeLn('bnez ', REGS[ra], ', ', imm);
         end;
+        %001101: printJFormat('call', ra, joffset);
+        %001010: printJFormat('jmp', ra, joffset);
+        %001001: decodeLoadStore(opcode, ra, rb, lssize, lsoffset);
+        %001100: decodeLoadStore(opcode, ra, rb, lssize, lsoffset);
+        %001011: writeLn('lih ', REGS[ra], ', ', imm);
+        %000110: printIFormat('ori', ra, rb, imm);
+        %001000: decodeLoadStore(opcode, ra, rb, lssize, lsoffset);
+        %000011: printIFormat('subi', ra, rb, imm);
+        %000100: printIFormat('subiu', ra, rb, imm);
+        %000111: printIFormat('xori', ra, rb, imm);
     else
         writeLn('???');
     end;
